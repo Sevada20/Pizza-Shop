d24431f16e5bb3ee5a659d0ca7445982a844268d
Sevada20 <poxosyansevada@gmail.com>
2023-05-20 01:28:50 +0400

    react-router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import "./scss/app.scss";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -9,9 +9,9 @@ const NotFoundBlock = () => {
         <span role="img" aria-label="Грустный">
           &#128546;
         </span>
-        <br />
-        <h1>Nothing found</h1>
       </h1>
+      <br />
+      <h1>Nothing found</h1>
       <p className={styles.description}>
         Sorry, this page is not available in our online store.
       </p>
